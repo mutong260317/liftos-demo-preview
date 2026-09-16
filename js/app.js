1fbf4b5efe46934a4fbe50f187367f3ac8068ab7
mutong260317 <mutong260317@github.com>
2026-09-16 19:10:39 +0800

    fix: restore mt-3 on plan start button

--- js/app.js
+++ js/app.js
@@ -363,7 +363,7 @@ LiftOS.UI = (() => {
           .join("")}
       </div>
       <button class="btn btn-secondary btn-block mt-3" onclick="App.setLibraryMode('addToPlan','${planId}')">+ 添加动作</button>
-      <button class="btn btn-primary btn-block btn-lg" onclick="App.startWorkoutFromPlan('${planId}')">用此计划开始</button>
+      <button class="btn btn-primary btn-block btn-lg mt-3" onclick="App.startWorkoutFromPlan('${planId}')">用此计划开始</button>
       <button class="btn btn-danger btn-block mt-2" onclick="App.confirmDeletePlan('${planId}')">删除计划</button>
     `;
     openSubpage("subpage-plan");
